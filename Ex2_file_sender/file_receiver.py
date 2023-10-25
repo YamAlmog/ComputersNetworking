914d73f6706c4ea775f3b4d3bcb837442d6c67c1
YamAlmog <yamalmog14@gmail.com>
2023-10-25 18:14:48 +0300

    fix all the CR

--- Ex2_file_sender/file_receiver.py
+++ Ex2_file_sender/file_receiver.py
@@ -1,50 +1,79 @@
 import socket
 
+PORT = 8820
+SERVER_IP = "127.0.0.1"
+CHUNK_SIZE = 1024
+FILE_NOT_FOUND = "NOFILE"
+END_THE_PROGRAM = "DONE"
+EMPTY_FILE= "Empty file"
+MAGIC_END_FILE_KEY = "$$$<END_OF_FILE>$$$"
 
-# CR: Where is main function??? you should always have a 'main' function as entry point to your program
-# also I DONT SEE ANY FUNCTIONS IN THIS FILE
+# connect and return socket
+def connect_to_server(socket):
+    try: 
+        socket = socket.socket()
+        socket.connect((SERVER_IP, PORT))
+        return socket
+    except:
+        raise Exception("Could not connect")
 
-
-# CR: Server IP SHOULD BE CONST, ALSO PORT
-my_socket = socket.socket()
-
-# CR: what happends if the server is not running? you should handle this case! and exit the program with a message to the user
-# all potential errors should be handled !
-my_socket.connect(("127.0.0.1", 8820))
-
-file_name = input("Input filename you want to get: \n")
-my_socket.send(file_name.encode())
-
-
-# output file should be named exactly as the input file name.
-# I want the ability to transfer any kind of file, not only text files.
-
-New_file_name = "output.txt"
-file = open(New_file_name, "w")
-print(f"{New_file_name} has created")
-
-
-# I want the program to ask users for file names to get from server, until the user enters 'exit' or 'quit'
-
-while True:
-    # 1. why do you need to decode the data? you should send the data as bytes and not as string
-    #    If I'm transferring a binary file, you will not be able to decode it as string (!)
-    # 2. you use 'while True' but when the file ends, you dont know when to stop reading from the socket
-    #    and this recv function blocks the code until it gets data from the socket (program will stuck here and never exit !)
-    data = my_socket.recv(1024).decode("utf-8")
-    if len(data) == 0:
-        break
-
-    # all MAGIC STRINGS SHOULD BE DEFINED AS CONSTANTS AT THE BEGINNING OF THE FILE
-    if data == "NOFILE":
-        print(f"There isn't file named: {file_name}")
-        break
-    else:
-        print(f"{data}")
-
-        # when do you close the file ??? you should close the file after you finish writing to it
-        file.write(data)
-        continue
+# returns file name 
+def get_user_request():
+   user_input = input("Input filename you want to get, don't forget add the suffix\n"
+                      "To exit the program, input the word 'DONE'\n "
+                      "Input your request here:")
+   return user_input
 
 
-my_socket.close()
+
+def get_file_from_server(socket, file_name):
+    # create copy file 
+    file = open(file_name, "w")
+    print(f"{file_name} has created")
+
+    # send file name to server to check if exsist
+    socket.send(file_name.encode())
+
+    # get chunk by chunk the file from server
+    # until the end of file is received
+    data = socket.recv(CHUNK_SIZE)
+
+    while data != MAGIC_END_FILE_KEY:
+        if data == EMPTY_FILE:
+            print("The requested file is empty")
+            break
+        elif data == FILE_NOT_FOUND:
+            raise Exception("File not found")
+        else:
+            file.write(data)
+            data = socket.recv(CHUNK_SIZE)
+            continue
+    
+    file.close()
+
+
+def main():
+
+    try:
+        server_socket = connect_to_server()
+    except Exception:
+        print("Connection Error: something went wrong, Apologies for the inconvenience.")
+        return
+
+    user_request= get_user_request()
+    while user_request != END_THE_PROGRAM: 
+
+        try:
+            get_file_from_server(server_socket, user_request)
+        except Exception:
+            print(f"There isn't file named: {user_request}")
+
+
+        user_request= get_user_request()
+
+    server_socket.close()
+
+
+
+
+
--- Ex2_file_sender/file_server.py
+++ Ex2_file_sender/file_server.py
@@ -5,15 +5,15 @@ import os
 PORT = 8820
 SERVER_IP = "127.0.0.1"
 BIND = (SERVER_IP, PORT)
-HEADER = 1024
+CHUNK_SIZE = 1024
+FILE_NOT_FOUND = "NOFILE"
+END_THE_PROGRAM = "DONE"
+EMPTY_FILE= "Empty file"
+MAGIC_END_FILE_KEY = "$$$<END_OF_FILE>$$$"
 server = socket.socket()
 server.bind(BIND)
-server.listen()
-print(f"[LISTENING] Server is listenin on...")
 
 
-# 1. Where is main function??? you should always have a 'main' function as entry point to your program
-#    also I DONT SEE ANY FUNCTIONS IN THIS FILE
 # 2. I want the server to accept request to get files from clients, until the server receives a "DONE" command
 # 3. Handle all potential errors that can happen in the program (try/except)
 
@@ -21,44 +21,46 @@ print(f"[LISTENING] Server is listenin on...")
 def send_file_to_receiver(user, filename):
     # Reading file and sending data to user
     file = open(filename, "r")
-    data = file.read()
-    if not data:
+    data = file.read(CHUNK_SIZE) #read the data chank by chank and then send it
+    
+    if len(data) == 0: #In case of empty file
         print(f"{filename} is empty file")
-
+        user.send(EMPTY_FILE.encode())
+    
     while data:
-        print(f"{data}")
-        user.send(str(data).encode())
+        user.send(data.encode()) # data could be any kind of data, not just text file
+        print(f"Sending the data is in progress...")
         data = file.read()  # read the next chunk of data from the file
 
     file.close()  # File is closed after data is sent
+    user.send("MAGIC_END_FILE_KEY".encode())
 
 
-def search_file(user_socket, file_name, search_path="."):
-    # you dont need to to do that . (I bet you dont understand what you did here, but you just copied it from stackoverflow)
-    # you can use os.path.exists(file_name) to check if the file exists (which is simpler and more readable)
-    # or you can use open() function with 'try/except' to check if the file exists
 
-    for root, dirs, files in os.walk(search_path):
-        if file_name in files:
-            print(f"{file_name} is exist")
+def main():
+    server.listen()
+    print(f"[LISTENING] Server is listenin on...")
 
-            send_file_to_receiver(user_socket, file_name)
+    (receiver, address) = server.accept()
+    print("Receiver is connected")
+
+    while True:
+        request_file = receiver.recv(CHUNK_SIZE)
+        
+        if request_file==END_THE_PROGRAM:
+            print("The program has ended")
+            break
+
+        elif os.path.exists(request_file):
+            print(f"{request_file} is exist")
+            send_file_to_receiver(receiver, request_file)
+            
 
         else:
-            # does not exist !!!!!!!  ()
-            print(f"{file_name} is Not exist")
-            # all MAGIC STRINGS SHOULD BE DEFINED AS CONSTANTS AT THE BEGINNING OF THE FILE
-            user_socket.send("NOFILE".encode())
-            return
+            print(f"{request_file} does not exist") 
+            receiver.send(FILE_NOT_FOUND.encode()) # the receiver will asked to try again or otherwise input the key word DONE
+            continue
 
 
-(receiver, address) = server.accept()
-print("Receiver is connected")
-# 1. why do you need to decode the data? you should send the data as bytes and not as string
-#    If I'm transferring a binary file, you will not be able to decode it as string (!)
-file_name = receiver.recv(1024).decode("utf-8")
-search_file(receiver, file_name, search_path=".")
-
-
-server.close()
-receiver.close()
+    server.close()
+    receiver.close()
